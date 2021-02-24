3f6f184c40b7a0c3fd580224fef72e6fee068b9c
Benson Lin <jack45j@gmail.com>
2021-02-24 10:01:03 +0800

    Initial Commit

---
+++ BSTabPageController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BSTabPageController
+//
+//  Created by 林翌埕-20001107 on 2021/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
